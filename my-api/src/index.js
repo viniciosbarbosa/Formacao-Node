134fb3efc090de35662b3286e9dd34589c4f339a
viniciosl <viniciosbarbosagit@gmail.com>
2024-06-04 20:24:27 -0300

    crud index

--- my-api/src/index.js
+++ my-api/src/index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (resquest, response) {
-  return response.json({
-    message: "Hello, node world!",
-  });
+app.get("/projects", function (resquest, response) {
+  return response.json(["Project 1", "Project 2"]);
+});
+
+app.post("/projects", function (resquest, response) {
+  return response.json(["Project 1", "Project 2", "Project 3"]);
+});
+
+app.put("/projects/:id", function (resquest, response) {
+  return response.json(["Project 4", "Project 2", "Project 1"]);
+});
+
+app.delete("/projects/:id", function (resquest, response) {
+  return response.json(["Project 2", "Project 1"]);
 });
 
 app.listen(3000, () => {

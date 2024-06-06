c97d221845eb5e08537c0b45906b83b83b4ddc60
viniciosl <viniciosbarbosagit@gmail.com>
2024-06-06 15:57:27 -0300

    atualizando o projeto com typescript

--- my-api/src/index.js
+++ my-api/src/index.js
@@ -1,19 +1,35 @@
 const express = require("express");
 const app = express();
 
-app.get("/projects", function (resquest, response) {
+app.use(express.json());
+
+app.get("/projects", function (request, response) {
+  const { title, owner, page } = request.query;
+
+  console.log(title, owner, page);
+
   return response.json(["Project 1", "Project 2"]);
 });
 
-app.post("/projects", function (resquest, response) {
+app.post("/projects", function (request, response) {
+  const { name, owner } = request.body;
+
+  console.log(name, owner);
+
   return response.json(["Project 1", "Project 2", "Project 3"]);
 });
 
-app.put("/projects/:id", function (resquest, response) {
+app.put("/projects/:id", function (request, response) {
+  const { id } = request.params;
+  const { name, owner } = request.body;
+  console.log(id, name, owner);
+
   return response.json(["Project 4", "Project 2", "Project 1"]);
 });
 
-app.delete("/projects/:id", function (resquest, response) {
+app.delete("/projects/:id", function (request, response) {
+  const { id } = request.params;
+
   return response.json(["Project 2", "Project 1"]);
 });
 
